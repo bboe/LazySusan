f1034bffd622a3e81adbfceea3a211441c37e1cb
Jeff Browne <jbrowne@cs.ucsb.edu>
2012-10-29 21:14:44 -0700

    Added three plugin functions to mod.py * BotPlaylist plugin for playlist manipulation (queue current song) * BotDJ can now /skip and /autoskip

--- lazysusan/plugins/mod.py
+++ lazysusan/plugins/mod.py
@@ -4,25 +4,47 @@ from lazysusan.plugins import CommandPlugin
 
 
 class BotDJ(CommandPlugin):
-    COMMANDS = {'/djdown': 'stop', '/djup': 'play'}
+    COMMANDS = {'/autoskip': 'auto_skip',
+                '/djdown': 'stop',
+                '/djup': 'play',
+                '/skip': 'skip_song'}
 
     @property
     def is_dj(self):
         return self.bot.bot_id in self.bot.dj_ids
 
+    @property
+    def is_playing(self):
+        return self.bot.bot_id == self.bot.api.currentDjId
+
     def __init__(self, *args, **kwargs):
         super(BotDJ, self).__init__(*args, **kwargs)
+        self.should_auto_skip = False
         self.register('add_dj', self.dj_update)
         self.register('deregistered', self.dj_update)
+        self.register('newsong', self.new_song)
         self.register('registered', self.dj_update)
         self.register('rem_dj', self.dj_update)
 
     @display_exceptions
+    @no_arg_command
+    def auto_skip(self, message, data):
+        """Toggle whether the bot should play anything."""
+        self.should_auto_skip = not self.should_auto_skip
+        if self.should_auto_skip:
+            self.bot.reply('I\'ll just keep this seat warm for you.', data)
+            if self.is_playing and len(self.bot.dj_ids) > 1:
+                self.bot.api.skip()
+        else:
+            self.bot.reply('I\'m back baby!', data)
+
+    @display_exceptions
     def dj_update(self, data):
-        # Ignore updates from the bot
         for user in data['user']:
             if self.bot.bot_id == user['userid']:
-                return
+                if data['command'] == 'rem_dj':
+                    self.should_auto_skip = False
+                return  # Ignore updates from the bot
 
         num_djs = len(self.bot.dj_ids)
         num_listeners = len(self.bot.listener_ids)
@@ -40,6 +62,13 @@ class BotDJ(CommandPlugin):
                 print 'Stepping up to DJ'
                 self.bot.api.addDj()
 
+    @display_exceptions
+    def new_song(self, data):
+        """Called when a new song starts playing."""
+        num_djs = len(self.bot.dj_ids)
+        if self.is_playing and self.should_auto_skip and num_djs > 1:
+            self.bot.api.skip()
+
     @moderator_required
     @no_arg_command
     def play(self, message, data):
@@ -50,6 +79,16 @@ class BotDJ(CommandPlugin):
             return self.bot.api.addDj()
         self.bot.reply('I can not do that right now.', data)
 
+    @display_exceptions
+    @no_arg_command
+    def skip_song(self, message, data):
+        """Ask the bot to skip the current song"""
+        if not self.is_playing:
+            self.bot.reply('I am not currently playing.', data)
+        else:
+            self.bot.api.skip()
+            self.bot.reply(':poop: I was just getting into it.', data)
+
     @moderator_required
     @no_arg_command
     def stop(self, message, data):
@@ -57,3 +96,15 @@ class BotDJ(CommandPlugin):
         if not self.is_dj:
             return self.bot.reply('I am not currently DJing.', data)
         self.bot.api.remDj()
+
+
+class BotPlaylist(CommandPlugin):
+    COMMANDS = {'/botq': 'queue_song'}
+
+    @display_exceptions
+    @no_arg_command
+    def queue_song(self, message, data):
+        """Request that the bot add the current song to her playlist."""
+        self.bot.reply("Cool tunes, daddio.", data)
+        self.bot.api.playlistAdd(self.bot.api.currentSongId, -1)
+        self.bot.api.bop()
