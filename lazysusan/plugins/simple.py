8d4e68db0f4e6cc41768f4a81d8a7f3117935f8e
Bryce Boe <bbzbryce@gmail.com>
2013-04-12 16:45:39 -0700

    Rename simple.Echo to simple.Talk and add /say command.

--- lazysusan/plugins/simple.py
+++ lazysusan/plugins/simple.py
@@ -3,12 +3,17 @@
 from lazysusan.plugins import CommandPlugin
 
 
-class Echo(CommandPlugin):
+class Talk(CommandPlugin):
 
     """A plugin that enables users to `speak` as the bot."""
 
-    COMMANDS = {'/echo': 'echo'}
+    COMMANDS = {'/echo': 'echo',
+                '/say': 'say'}
 
     def echo(self, message, data):
-        """Repeat everything everything after /echo."""
+        """Repeat everything after /echo to the same stream."""
         self.bot.reply(message, data)
+
+    def say(self, message, data):
+        """Repeat everything after /speak to the bot's current room."""
+        self.bot.api.speak(message)
